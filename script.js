b028e83bcf2a545756a7d5a9430670deb42344bf
Ann Chen <annchen@andrew.cmu.edu>
2016-10-18 23:09:01 -0500

    submit on enter, manifest accepts jquery script, need to validate for blanks

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+
 document.addEventListener('DOMContentLoaded', function() {
 	var displayMemories = document.getElementById("display-memories")
 	displayMemories.addEventListener('click', function(){
-			document.getElementById("showMemories").innerHTML+=document.getElementById("thetext").value + '<br>'
+			if($("#enterText")){
+				console.log($("#enterText"));
+			document.getElementById("showMemories").innerHTML+=document.getElementById("enterText").value + '<br>';
+			document.getElementById("enterText").value = "";
+		}
+	});
 
-	})
+
+	$("#enterText").keydown(function(event){
+	if(event.which==13){
+		event.preventDefault();
+		$("#display-memories").click();
+	}
+
+});
 });
