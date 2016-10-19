3671d1936319e35eb39f5a86ef6cda1d8098667d
Ann Chen <annchen@andrew.cmu.edu>
2016-10-18 23:14:40 -0500

    checks for blank text

--- script.js
+++ script.js
@@ -2,18 +2,17 @@
 document.addEventListener('DOMContentLoaded', function() {
 	var displayMemories = document.getElementById("display-memories")
 	displayMemories.addEventListener('click', function(){
-			if($("#enterText")){
-				console.log($("#enterText"));
 			document.getElementById("showMemories").innerHTML+=document.getElementById("enterText").value + '<br>';
 			document.getElementById("enterText").value = "";
-		}
 	});
 
 
 	$("#enterText").keydown(function(event){
 	if(event.which==13){
+		if ($("#enterText").val().length>0 ){
 		event.preventDefault();
 		$("#display-memories").click();
+	}
 	}
 		
 
