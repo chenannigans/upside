ff1c0aaf6f52caec746b19fea1be7ac3de309879
Ann Chen <annchen@andrew.cmu.edu>
2016-10-21 01:37:02 -0500

    script currently in shambles, TODO: figure out structure for storing memories, displaymemories should iterate through array, more jquery restructuring

--- script.js
+++ script.js
@@ -1,30 +1,55 @@
 $(document).ready(function() {
 
-	displayMemories();
+	// displayMemories();
 	enterMemories();	
+	// loadMemories();
 
 
-
-
+	console.log(chrome.storage);
 
 	function displayMemories(){
-		var displayMemories = document.getElementById("display-memories")
-		displayMemories.addEventListener('click', function(){
-				document.getElementById("showMemories").innerHTML=document.getElementById("enterText").value + '<br>' + document.getElementById("showMemories").innerHTML ;
-				document.getElementById("enterText").value = "";
-		});
+
+		// $("#showMemories").append($("#enterText").val());
+		// $("#enterText").val("");
+
+
+
+		// var displayMemories = document.getElementById("display-memories")
+		// displayMemories.addEventListener('click', function(){
+		// 		document.getElementById("showMemories").innerHTML=document.getElementById("enterText").value + '<br>' + document.getElementById("showMemories").innerHTML ;
+		// 		document.getElementById("enterText").value = "";
+		// });
+	}
+
+	function loadMemories(){
+		var themem = chrome.storage.sync.get("memory");
+		console.log(themem);
+
+		$("#memoryLog").append(themem);
 	}
 
 
 	function enterMemories(){
-	$("#enterText").keydown(function(event){
-		if(event.which==13){
-			if ($("#enterText").val().length>0 ){
-				event.preventDefault();
-				$("#display-memories").click();
+		$("#enterText").keydown(function(event){
+			if(event.which==13){
+				if ($("#enterText").val().length>0 ){
+					event.preventDefault();
+
+					var newMemory = $("#enterText").val();
+					storeData('memory', newMemory);
+				
+
+					// displayMemories();
+					$("#enterText").val("");
+
+				}
 			}
-		}
-	});
+		});
+	}
+
+	function storeData(key,val){
+		chrome.storage.sync.set({key: val});
+		console.log("stored ho");
 	}
 
 
