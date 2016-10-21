32fa83e5b5186191888a780cb633c92f5d89acdc
Ann Chen <annchen@andrew.cmu.edu>
2016-10-21 00:08:59 -0500

    js restructuring

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
+$(document).ready(function() {
 
-document.addEventListener('DOMContentLoaded', function() {
-	var displayMemories = document.getElementById("display-memories")
-	displayMemories.addEventListener('click', function(){
-			document.getElementById("showMemories").innerHTML=document.getElementById("enterText").value + '<br>' + document.getElementById("showMemories").innerHTML ;
-			document.getElementById("enterText").value = "";
-	});
+	displayMemories();
+	enterMemories();	
 
 
+
+
+
+	function displayMemories(){
+		var displayMemories = document.getElementById("display-memories")
+		displayMemories.addEventListener('click', function(){
+				document.getElementById("showMemories").innerHTML=document.getElementById("enterText").value + '<br>' + document.getElementById("showMemories").innerHTML ;
+				document.getElementById("enterText").value = "";
+		});
+	}
+
+
+	function enterMemories(){
 	$("#enterText").keydown(function(event){
-	if(event.which==13){
-		if ($("#enterText").val().length>0 ){
-		event.preventDefault();
-		$("#display-memories").click();
+		if(event.which==13){
+			if ($("#enterText").val().length>0 ){
+				event.preventDefault();
+				$("#display-memories").click();
+			}
+		}
+	});
 	}
-	}
-		
 
-});
+
+
+
 });
