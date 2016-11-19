50653c559543834996d2190aa303b6bde8f8a6eb
Amy Li <amyli@andrew.cmu.edu>
2016-11-19 14:37:44 -0500

    ugly tag cloud added to analysis tab

--- script.js
+++ script.js
@@ -12,6 +12,26 @@ $(document).ready(function() {
 
 	
 
+	window.onload = function() {
+    try {
+      TagCanvas.Start('myCanvas');
+    } catch(e) {
+      // something went wrong, hide the canvas container
+      document.getElementById('myCanvasContainer').style.display = 'none';
+    }
+  };
+
+  if( ! $('#myCanvas').tagcanvas({
+     textColour : '#ffffff',
+     outlineThickness : 1,
+     maxSpeed : 0.03,
+     depth : 0.75
+   })) {
+     // TagCanvas failed to load
+     $('#myCanvasContainer').hide();
+   }
+
+
 	function getData(){
 	
 			chrome.storage.sync.get(function(data){
